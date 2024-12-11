c6ac02a346d188eab6d4128bf631612f84931313
Esteban Rodriguez <Esteban.Rodriguez@palmbeachschools.org>
2024-12-11 20:03:48 +0000

    1. set up db connection.

---
+++ SolanaPaper/Data/Models/Users.cs
@@ -0,0 +1,16 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System.Text.Json.Serialization;
+
+namespace SolanaPaper.Data.Models
+{
+    public class Users
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; }
+
+
+
+    }
+}
---
+++ SolanaPaper/Data/Services/MongoDBService.cs
@@ -0,0 +1,26 @@
+﻿using SolanaPaper.Data.Models;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+using MongoDB.Bson;
+
+
+namespace SolanaPaper.Data.Services
+{
+    public class MongoDBService
+    {
+        private readonly IMongoCollection<Users> _usersCollection;
+
+        public MongoDBService(IOptions<MongoDBSettings> mongoDBSettings) 
+        {
+            MongoClient client = new MongoClient(mongoDBSettings.Value.ConnectionURI);
+            IMongoDatabase database = client.GetDatabase(mongoDBSettings.Value.DatabaseName);
+            _usersCollection = database.GetCollection<Users>(mongoDBSettings.Value.CollectionName);
+        }
+
+        public async Task CreateAsync(Users users) 
+        {
+            await _usersCollection.InsertOneAsync(users);
+            return;
+        }
+    }
+}
